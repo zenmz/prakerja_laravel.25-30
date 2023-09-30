3680598f3bb59dcec690b1fe9b1246f0428b6b03
zenmz <zainiqbalmz@gmail.com>
2023-09-30 13:52:38 +0700

    update 30/9

---
+++ resources/views/upload/tampil.blade.php
@@ -0,0 +1,30 @@
+@extends('template')
+@section('main')
+    <h1>Upload File</h1>
+    <form action="{{ route('upload.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="">
+            <div class="form-group">
+                <label>Example file input</label>
+                <input type="file" class="form-control-file" name="image">
+            </div>
+            <button class="btn btn-success">Simpan</button>
+        </div>
+    </form>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $item)
+                <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td><img src="{{ asset('storage/' . $item->image) }}" alt="" width="250px"></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
